94c575767744d45c3e305829894bbdcc0f173815
Benji Wiebe <benjiwiebe14@gmail.com>
2021-08-02 15:14:35 -0500

    Move variables out of code

--- compare.sh
+++ compare.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
-host=server
-file=INVENT
+# shellcheck disable=SC2162
+file=TRANSACT.IDX
+file2=TRANSACT
+srchost=vm
+srcdir=/mdbase/loc1
+destdir=/nas/Documents/Benji/compare
 echo Make sure /nas is read-write
 echo Press ENTER to copy the first file
 read
-sudo scp $host:/mdbase/loc1/$file /nas/Documents/Benji/compare/A
+sudo scp $srchost:$srcdir/$file $destdir/A1
+test x$file2 = x || sudo scp $srchost:$srcdir/$file2 $destdir/A2
 echo Press ENTER to copy the next file
 read
-sudo scp $host:/mdbase/loc1/$file /nas/Documents/Benji/compare/B
+sudo scp $srchost:$srcdir/$file $destdir/B1
+test x$file2 = x || sudo scp $srchost:$srcdir/$file2 $destdir/B2
 echo Done
