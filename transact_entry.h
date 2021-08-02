5425e7040b8f90da80010123abed4c75ec379102
Benji Wiebe <benjiwiebe14@gmail.com>
2021-08-02 15:08:40 -0500

    defines and includes

--- transact_entry.h
+++ transact_entry.h
@@ -1,17 +1,23 @@
 #ifndef _TRANSACT_ENTRY_INCLUDED_
 #define _TRANSACT_ENTRY_INCLUDED_
 
+#include <inttypes.h>
+
 #define TYPE_MEMO	'9'
 #define TYPE_USED	'7'
 #define TYPE_CORE	'5'
 #define TYPE_REMAN	'3'
 #define TYPE_NEW	'1'
 
-struct invent_entry {
+#define TRANSACT_RECORD_SIZE	128
+#define TRANSACT_FILE_SKIP		128
+
+
+struct transact_entry {
 	char part_number[21]; // length 20, offset 0
 	char vendor[4]; // length 3, offset 30
 	uint8_t type; // length 1, offset 35
-	time_t when; // length 4, offset 44
+	uint32_t when; // length 4, offset 44
 	char desc[13]; // length 12, offset 50
 	double change; // length 8, offset 62
 	double newqty; // length 8, offset 70
