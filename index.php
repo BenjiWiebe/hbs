ec7d75e35c305f204c84d9ab9789032b6a05596d
Benji Wiebe <benjiwiebe14@gmail.com>
2023-07-31 10:04:53 -0500

    Added search by stock status to web interface

--- index.php
+++ index.php
@@ -18,8 +18,27 @@ else
 	$input_name = "search";
 	$label_text = "Part number: ";
 	$search_by = "part";
-	?><input type="checkbox" form="searchform" name="partialm" id="partialm" <?=$_REQUEST["partialm"] == 'on' ? "checked" : ""?>></input><label style="text-decoration: underline;user-select:none;padding:" for="partialm">Partial match</label><p></p><?php
+	?><input type="checkbox" form="searchform" name="partialm" id="partialm" <?=$_REQUEST["partialm"] == 'on' ? "checked" : ""?>></input><label style="text-decoration: underline;user-select:none;padding:" for="partialm">Partial match</label><?php
 }
+	$all_checked = "";
+	$normal_checked = "";
+	$onhand_checked = "";
+	$out_checked = "";
+	switch($_REQUEST['stock']) {
+		case 'onhand': $onhand_checked = ' checked'; break;
+		case 'normalstock': $normal_checked = ' checked'; break;
+		case 'out': $out_checked = ' checked'; break;
+		case 'all':
+		default:
+			$all_checked = ' checked';
+	}
+?>
+<input type="radio" form="searchform" name="stock" id="all" value="all"<?=$all_checked?>></input><label for="all">All parts</label>
+<input type="radio" form="searchform" name="stock" id="normalstock" value="normalstock"<?=$normal_checked?>></input><label for="normalstock">Normally stocked</label>
+<input type="radio" form="searchform" name="stock" id="out" value="out"<?=$out_checked?>></input><label for="out">Out of stock</label>
+<input type="radio" form="searchform" name="stock" id="onhand" value="onhand"<?=$onhand_checked?>></input><label for="onhand">On hand</label>
+<?php
+print '<p></p>';
 print '<label for="search">' . $label_text . '</label>';
 if($search_by == "vendor") print '<input type="search" id="search" name="search" autofocus></input>';
 if($search_by == "part") print '<input type="search" id="search" name="search" autofocus></input>';
@@ -72,9 +91,32 @@ if(strlen($rq))
 		if($rq[0] == '/' || $_REQUEST['partialm'] == 'on') {
 			if($rq[0] == '/')
 				$rq = substr($rq, 1);
-			$j = find_results($rq, $search_by, true);
+			$rawresults = find_results($rq, $search_by, true);
 		} else {
-			$j = find_results($rq, $search_by);
+			$rawresults = find_results($rq, $search_by);
+		}
+
+		function check_stock_oh($i) {
+			return $i->onhand > 0;
+		}
+		function check_stock_normal($i) {
+			return $i->bin != 'SP ORD';
+		}
+		function check_stock_out($i) {
+			if($i->bin == 'SP ORD')
+				return false;
+			if($i->onhand > 0)
+				return false;
+			return true;
+		}
+		if($_REQUEST['stock'] == 'onhand') {
+			$j = array_filter($rawresults, "check_stock_oh");
+		} else if($_REQUEST['stock'] == 'normalstock') {
+			$j = array_filter($rawresults, "check_stock_normal");
+		} else if($_REQUEST['stock'] == 'out') {
+			$j = array_filter($rawresults, "check_stock_out");
+		} else {
+			$j = $rawresults;
 		}
 
 		// calculate the time it took, and the number of results
